55f80b43f75a8162e7e92746e4b27509d8fc5593
pasuya1981 <pasuya1234@hotmail.com>
2014-04-01 21:06:36 +0800

    Finish sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  	user = User.find_by_email(params[:session][:email])
+  	if user && user.authenticate(params[:session][:password])
+  	  # TODO: sign in user and redirect to the user's show page.
+      sign_in(user)
+      redirect_to user
+  	else
+  	  flash.now[:error] = "郵件/密碼 錯誤!"
+  	  flash.now[:notice] = "請登入"
+  	  render 'new'
+  	end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
+end
